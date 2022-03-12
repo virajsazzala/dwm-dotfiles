79f9a073a12a83e8573fbc26e264456e26038481
exvynai <vir200319@gmail.com>
2022-03-12 21:43:55 +0530

    cleaned and fixed the statusbar scripts bug

--- install.sh
+++ install.sh
@@ -1,83 +1,82 @@
 #!/bin/bash
 
-#This script installs exvynai's dwm config
-#Author: exvynai
-#Repo: https://github.com/exvynai/dwm-dotfiles
-#Start date: 21 Feb 2022
-
 echo "Starting installation!"
-sleep 5
+sleep 3
 
 read -p "Enter your username: " username
 
-#packages to install (pacman)
+#installing pacman packages
 cd
 sudo pacman -Syy
 sudo pacman -Syu
-sudo pacman -S --needed base-devel vim xfce4-terminal git scrot nitrogen sxiv mpv firefox emacs pcmanfm discord ttf-font-awesome ttf-joypixels python-pywal obs-studio xorg-server xorg-xinit xorg-xkill xorg-xsetroot xorg-xbacklight xorg-xprop noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-jetbrains-mono zathura zathura-pdf-mupdf ffmpeg imagemagick fzf man-db xwallpaper unclutter xclip maim zip unzip unrar p7zip xdotool papirus-icon-theme brightnessctl dosfstools ntfs-3g sxhkd pulseaudio vim arc-gtk-theme rsync dash xcompmgr libnotify dunst slock jq dhcpcd networkmanager rsync pamixer xdg-user-dirs picom
-cd dwm-dotfiles
+sudo pacman -S --needed base-devel vim xfce4-terminal scrot nitrogen sxiv mpv pcmanfm discord ttf-font-awesome ttf-joypixels python-pywal obs-studio xorg-server xorg-xinit xorg-xkill xorg-xsetroot xorg-xbacklight xorg-xprop noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-jetbrains-mono zathura zathura-pdf-mupdf ffmpeg imagemagick fzf man-db xwallpaper unclutter xclip maim zip unzip unrar p7zip xdotool papirus-icon-theme brightnessctl dosfstools ntfs-3g sxhkd pulseaudio arc-gtk-theme rsync pamixer dash xcompmgr libnotify dunst slock jq dhcpcd networkmanager xdg-user-dirs picom 
 
 echo "packages installed!"
-sleep 5
+sleep 3
 
-#gotta install yay
-cd /opt
+#yay installation
+cd ~/opt
 sudo git clone https://aur.archlinux.org/yay-git.git
 sudo chown -R $username:$username ./yay-git
 cd yay-git
 makepkg -si
 
-echo "yay installed!"
-sleep 5
+echo "Yay installed!"
+sleep 3
 
-#yay-packages
+#installing yay packages
 cd
-yay -S ani-cli
+yay -S ani-cli 
+yay -S brave-bin
 
+#cp xinitrc and bashrc
 cd ~/dwm-dotfiles
-
-#get xinitrc and bashrc
 cp myxinitrc ~/.xinitrc
 cp mybashrc ~/.bashrc
-
-echo "done copying bash and xinitrc!"
-sleep 5
-
-#get wallpaper
 cd 
-mkdir pix
-cp ~/dwm-dotfiles/girl-red-mask.jpg ~/pix
-echo "done copying wallpaper!"
-sleep 5
 
-cd ~/dwm-dotfiles
+echo "done copying bashrc and xinitrc!"
+sleep 3
 
+#clone wallapers repo
 cd
-cd .local
-mkdir bin
-cd ~/dwm-dotfiles
+mkdir ~/pix && cd ~/pix
+sudo git clone https://github.com/exvynai/wals.git
+cd
 
-#cp scripts to .local/bin
-cd scripts
-cp battery brightness.sh internet time.sh volume.sh iam.sh ~/.local/bin
-cd ..
+echo "done getting wallpapers!"
+sleep 3
+
+#move scripts
+mkdir -p ~/.home/bin
+cd ~/dwm-dotfiles/scripts
+cp battery internet time.sh volume.sh -t ~/.local/bin
+cd ~/.local/bin
+chmod +x battery internet time.sh volume.sh
+cd
 
 echo "done copying scripts!"
-sleep 5
+sleep 3
 
-#make directories
+#make all imp dirs
+cd ~/dwm-dotfiles
 cd dwm && sudo make && sudo make install && cd ..
 cd dmenu && sudo make && sudo make install && cd ..
 cd dwmblocks && sudo make && sudo make install && cd ..
 cd st && sudo make && sudo make install && cd ..
-echo "done compiling and building files!"
-sleep 5
+cd
 
-#changing pywal on terminal
-wal -i ~/pix/girl-red-mask.jpg
+echo "done making all dirs!"
+sleep 3
+
+#terminal themeing with pywal
+cd
+wal -i ~/pix/wals/makimaf\ \(1\).jpg
+
 echo "terminal theme changed!"
-sleep 5
+sleep 3
 
 #post installation
-echo "you need to choose your wallpaper using nitrogen and reboot!"
+echo "all done, now just use nitrogen to set your wallpaper and reboot!"
+
 
